123ca51486c159a1c8e7370bbee926f3fdbbbeca
DonalHowe <donalhowe@gmail.com>
2021-04-19 21:00:23 +0100

    pwa

--- jsScripts/register.js
+++ jsScripts/register.js
@@ -2,6 +2,6 @@ window.onload=()=>{
     'use strict';
     if('serviceWorker' in navigator){
         navigator.serviceWorker
-        .register('/serviceWorker.js')
+        .register('./serviceWorker.js')
     }
 }
