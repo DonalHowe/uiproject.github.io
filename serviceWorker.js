24d7662a87275918d800048564250d1b9a482642
DonalHowe <donalhowe@gmail.com>
2021-04-21 19:20:25 +0100

    dkfgjkjlsd

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,28 @@
-
 var cacheName = 'DonalHowe-Pwa'
 var filesToCache = [
-    '/',
-    '/index.html',
-    '/newLoader.html',
-    '/css/style.css',
-    '/jsScripts/game.js',
-    '/jsScripts/loaderScript.js'
+    './',
+    './index.html',
+    './newLoader.html',
+    './css/style.css',
+    './jsScripts/game.js',
+    './jsScripts/loaderScript.js',
+    './gameplay.html'
 ];
 
-self.addEventListener('install',function(e)
-{
-    e.waitUntil(caches.open(cacheName).then(function(cache)
-        {
-        return cache.addAll(filesToCache);
-        })
-    );
+/* Cache contents when Offline See Cache */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
 });
-self.addEventListener('fetch',function(e){
-    e.respondwith(
-        caches.match(e.request).then(function(response){
-            return response || fetch(e.request);
-        })
+
+/* Serve cached content when offline, examine Cache Storage */
+self.addEventListener('fetch', function(e) {
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
     );
-});
+  });
